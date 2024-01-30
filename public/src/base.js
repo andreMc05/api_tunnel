01ac9f733df28dafe317aec31efc3af6f060e135
Andre McGruder <andremc05@gmail.com>
2024-01-30 13:25:53 -0600

    DOM templating and styling base set.

---
+++ public/src/base.js
@@ -0,0 +1,104 @@
+const api_tunnel = (function (){
+    const apiTemplateString = `
+    <div>
+        <button class="api-row">
+            <span class="api-method"></span>
+            <div class="method-details">
+                <span class="method-details--description"></span>
+            </div>
+        </button>
+        <form> 
+            <input>
+            <button>Submit</button>
+        </form>
+        <pre id="response"></pre>
+    </div>
+    `;
+    const pathDescription = `<span class="method-details--description"></span>`;
+    const template = document.createElement('template');
+
+    loadApiEndpoints();
+
+    // Fetch and display API endpoints
+    async function loadApiEndpoints() {
+        const response = await fetch('/apis/api-endpoints.json');
+        const apiEndpoints = await response.json();
+        const list = document.getElementById('apiList');
+        createApiDom(apiEndpoints);
+    }
+    
+    function createDomFromStrings(str){
+        const template = document.createElement('template');
+        template.innerHTML = sanitizeHtml(str);
+        return template.content.cloneNode(true);
+    }
+
+    function createApiDom(data){
+        const node = document.createRange().createContextualFragment(apiTemplateString);
+        const template = document.createDocumentFragment();
+        data.forEach(endpoint => {
+            const templateMain = createDomFromStrings(apiTemplateString);
+            const templateRow = templateMain.querySelector('.api-row');
+            const templateMethod = templateMain.querySelector('.api-method');
+            const templateMethodDetails = templateMain.querySelector('.method-details>span');
+    
+            
+            templateMethod.textContent = `${endpoint.method}`;
+            if(!!endpoint.parameter){
+                templateMethodDetails.textContent = `${endpoint.path}/${endpoint.parameter}`;
+            } else {
+                templateMethodDetails.textContent = `${endpoint.path}`;
+            }
+    
+            if(!!endpoint.description){
+                const enpointDesc = createDomFromStrings(pathDescription).querySelector('.method-details--description');
+                enpointDesc.textContent = `${endpoint.description}`;
+                templateMethodDetails.appendChild(enpointDesc);
+            }
+    
+            switch(endpoint.method) {
+                case 'GET':
+                    templateRow.classList.add('api-row--get');
+                    templateMethod.classList.add('api-method--get');
+                    break;
+                case 'POST':
+                    templateRow.classList.add('api-row--post');
+                    templateMethod.classList.add('api-method--post');
+                    break;
+                case 'PUT':
+                    templateRow.classList.add('api-row--put');
+                    templateMethod.classList.add('api-method--put');
+                    break;
+                case 'DELETE':
+                    templateRow.classList.add('api-row--delete');
+                    templateMethod.classList.add('api-method--delete');
+                    break;
+                default:
+                    console.log(`There was no color matching found for ${color}.`);
+            }
+    
+            template.appendChild(templateMain);
+            document.querySelector('#apiList').appendChild(templateMain);
+        });
+    
+        document.querySelector('#apiList').appendChild(template);
+    }
+
+    function sanitizeHtml(htmlString) {
+        // Remove script tags and their content
+        htmlString = htmlString.replace(/<script[^>]*>([\S\s]*?)<\/script>/gmi, '');
+    
+        // Remove event handlers from tags
+        htmlString = htmlString.replace(/ on\w+="[^"]*"/g, '');
+    
+        // Remove javascript: from href and action attributes
+        htmlString = htmlString.replace(/(href|action)="javascript:[^"]*"/g, '');
+    
+        return htmlString;
+    }
+
+    function toggleInput(){
+        console.log('---------------------- fetch data', this.parent);
+        console.dir(this);
+    }
+}(window.apiTunnel = window.apiTunnel || {} ));
